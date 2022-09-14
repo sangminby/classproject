4a1957ad83b436e28d123a6ca90cbfcc8e2523ba
sangminby <113090480+sangminby@users.noreply.github.com>
2022-09-14 17:04:21 +0900

    이항연산자
    산술연산자, 문자타입의 증감연산

---
+++ firstjava/src/Chapter03/CatingOperator.java
@@ -0,0 +1,22 @@
+package Chapter03;
+
+public class CatingOperator {
+
+	public static void main(String[] args) {
+		
+		byte num1 = 10;
+		byte num2 = 20;
+		
+		// 기본형 / 참조형
+		// 기본형
+		// 숫자 문자 논리값
+		// 숫자 - 정수 문자 논리값
+		// 정수 - byte short int long, 실수 - float double, 문자 - char, 논리값 - boolaen
+		
+		byte result = (byte)(num1 + num2);   // byte + byte -> int + int = int
+		
+		
+		
+		
+	}
+}
