552fe1696460ac144d804e1bd2142038e4836292
sangminby <113090480+sangminby@users.noreply.github.com>
2022-09-14 16:19:55 +0900

    연산자 세부코드
    연산자 반환

---
+++ firstjava/src/Chapter03/PostfixOperator.java
@@ -0,0 +1,22 @@
+package Chapter03;
+
+public class PostfixOperator {
+
+	
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		
+		System.out.println("num1 : " + num1);
+		
+		int num2 = num1++;   // num2 = num1 -> num1 + num1 + 1
+				             // num2 = 5, num1 = 6
+		
+		System.out.println("num1 : " + num1);
+		System.out.println("num2 : " + num2);
+		
+		System.out.println("num1 : " + num1++);   // 6
+		
+		
+	}
+}
