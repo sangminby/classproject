c7b856691aca5b3257196bd1a048a077bf2747eb
sangminby <113090480+sangminby@users.noreply.github.com>
2022-09-14 17:04:43 +0900

    단인연산자
    단일연산자

--- firstjava/src/Chapter03/PostfixOperator.java
+++ firstjava/src/Chapter03/PostfixOperator.java
@@ -9,14 +9,13 @@ public class PostfixOperator {
 		
 		System.out.println("num1 : " + num1);
 		
-		int num2 = num1++;   // num2 = num1 -> num1 + num1 + 1
+		int num2 = num1++;   // num2 = num1 -> num1 = num1 + 1
 				             // num2 = 5, num1 = 6
 		
 		System.out.println("num1 : " + num1);
 		System.out.println("num2 : " + num2);
 		
 		System.out.println("num1 : " + num1++);   // 6
-		
 		
 	}
 }
