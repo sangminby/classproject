e1d101afc12716c5659bdf4a07d33b7d3d80ac1b
sangminby <113090480+sangminby@users.noreply.github.com>
2022-09-15 10:37:25 +0900

    Create RelationlOperator.java

---
+++ firstjava/src/Chapter03/RelationlOperator.java
@@ -0,0 +1,36 @@
+package Chapter03;
+
+public class RelationlOperator {
+
+	public static void main(String[] args) {
+		
+		// 관계연산자 ( 비교연산자 )
+		// >  <  >=  <=  ==  !=
+		
+		int num1 = 30;
+		int num2 = 200;
+		
+		System.out.println(num1 > num2);   // true
+		System.out.println(num1 < num2);   // false
+		System.out.println(num1 >= num2);   // true
+		System.out.println(num1 <= num2);   // false
+		System.out.println(num1 == num2);   // false
+		System.out.println(num1 != num2);   // true
+		
+		if(num1 > num2) {
+			// 참일 때 처리되는 블록
+			System.out.println("num1이 num2보다 큽니다.");
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
