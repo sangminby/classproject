08d6f0ffbc04b06a09ef02c7b5d4626d1f38072d
sangminby <113090480+sangminby@users.noreply.github.com>
2022-09-30 12:30:52 +0900

    Project03

---
+++ OOPMiniProject/src/Ver03/CompanyContact.java
@@ -0,0 +1,52 @@
+package Ver03;
+
+public class CompanyContact extends Contact {
+
+//	① CompanyContact 
+//	회사, 거래처의 정보를 저장하는 하위 클래스를 정의합니다.
+//	- 회사이름, 부서이름, 직급 변수 추가
+//	- 정보를 출력하는 메소드를 오버라이딩 해서 추가된 정보를 추가해서 출력
+
+	private String company;
+	private String division;
+	private String manager;
+	
+	
+	public CompanyContact(String name, String number, String email, String address, String birthday, String group) {
+		super(name, number, email, address, birthday, group);
+	}
+
+
+	public CompanyContact(
+			String name, String number, String email, 
+			String address, String birthday, String group,
+			String company, String division, String manager) {
+		super(name, number, email, address, birthday, group);
+		
+		this.company = company;
+		this.division = division;
+		this.manager = manager;
+	}
+
+
+	@Override
+	public void printData() {
+		super.printData();
+		System.out.println("회사이름 : " + this.company);
+		System.out.println("부서이름 : " + this.division);
+		System.out.println("직급 : " + this.manager);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		Contact contact = new CompanyContact(
+				"Son", "010-2323-5656", "son@gmail", 
+				"London", "2000-10-12", "직장동료",
+				"토트넘", "선수", "없음");
+		
+		contact.printData();
+		
+	}
+	
+}
---
+++ OOPMiniProject/src/Ver03/Contact.java
@@ -0,0 +1,99 @@
+package Ver03;
+
+// Contact 클래스의 용도 ? -> 데이터 저장
+// 인스턴스 생성 -> 변수의 메모리 생성 -> 데이터 저장
+class Contact {
+	
+	/*	저장 정보
+ 		이름
+ 		전화번호
+ 		이메일
+ 		주소
+ 		생일
+ 		그룹
+			* 변수들은 직접 참조를 막아 캡슐화 처리를 하도록 해봅시다.
+	 */
+	private String name;		// 친구의 이름
+	private String number;		// 번호
+	private String email;		// 메일
+	private String address;		// 주소
+	private String birthday;	// 생일
+	private String group;		// 그룹
+	
+	public Contact() {}
+	
+	public Contact(String name, String number, String email, String address, String birthday, String group) {
+		this.name = name;
+		this.number = number;
+		this.email = email;
+		this.address = address;
+		this.birthday = birthday;
+		this.group = group;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	
+	// 위 데이터를 출력하는 기능
+	public void printData() {
+		System.out.println();
+		System.out.println("저장정보----------------");
+		System.out.println("이름 : " + this.name);
+		System.out.println("번호 : " + this.number);
+		System.out.println("메일 : " + this.email);
+		System.out.println("주소 : " + this.address);
+		System.out.println("생일 : " + this.birthday);
+		System.out.println("그룹 : " + this.group);
+	}
+	
+	@Override
+	public String toString() {
+		return "Contact [name=" + name + ", number=" + number + ", email=" + email + ", address=" + address
+				+ ", birthday=" + birthday + ", group=" + group + "]";
+	}
+	
+	public static void main(String[] args) {
+		
+		Contact contact = new Contact();
+		
+	
+		
+	}
+}
+
