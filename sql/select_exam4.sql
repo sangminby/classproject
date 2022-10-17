8dc461e53a6fea337f19aac3aa37a04b9ed363a5
LeeSangMin <sangminby123@naver.com>
2022-10-17 21:47:05 +0900

    select_exam4

--- sql/select_exam4.sql
+++ sql/select_exam4.sql
@@ -6,12 +6,12 @@ from orders
 where custid = 1;
 
 -- (4) 박지성이 구매한 도서의 수 (박지성의 고객번호는 1번으로 놓고 작성)
-select count(*) 
+select count(saleprice) 
 from orders
 where custid = 1;
 
 -- (1) 마당서점 도서의 총개수
-select count(*) from book;
+select count(bookname) from book;
 
 -- (2) 마당서점에 도서를 출고하는 출판사의 총개수
 select count(distinct publisher) from book;
