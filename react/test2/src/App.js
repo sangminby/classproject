b539e9689e910ffc4cf15649e24057e218ff11ce
sangminby <113090480+sangminby@users.noreply.github.com>
2022-12-27 12:31:25 +0900

    react

--- react/test2/src/App.js
+++ react/test2/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React000000
+          Learn React
         </a>
       </header>
     </div>
